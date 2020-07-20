2e9f7af86417357210a006b2a85327ff2e5070c1
Felipe Sette <felipe_sette@outlook.com>
2020-07-20 17:55:37 -0300

    Fazendo cálculos com JavaScript

---
+++ calculos.js
@@ -0,0 +1,14 @@
+/* Criar um programa que calcula a média das notas entre alunos e envia 
+mensagem do cálculo da média. */
+
+const aluno01 = 'Felipe'
+const aluno02 = 'Guilherme'
+const aluno03 = 'Fabiana'
+
+const NotaAluno01 = 9.8
+const NotaAluno02 = 3
+const NotaAluno03 = 10
+
+const media = ( NotaAluno01 + NotaAluno02 + NotaAluno03 ) / 3
+
+console.log(media)
