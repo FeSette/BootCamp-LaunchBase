6b8f8ad499a5391f1a0033123be0c872103c17be
Felipe Sette <67072271+FeSette@users.noreply.github.com>
2020-07-20 18:58:16 -0300

    Aprendendo a fazer comentários

---
+++ Works/comentarios.js
@@ -0,0 +1,11 @@
+// Vamos aprender como fazer comentários no código sem interferir no programa.
+
+// Podemos ultilizar os comentários de duas formas:
+// com "//", que comenta apenas a linha onde estão, e o "/* */". que comenta 
+// todo o conteúdo entre eles.
+
+// Apenas uma linha
+
+/* TODO O CONTEÚDO 
+   DE QUANTAS LINHAS
+   ESTIVEREM ENTRE ELES */
