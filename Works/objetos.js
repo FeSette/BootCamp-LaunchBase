f4b806eb4586ea8af9f6984698abe60d895d99c1
Felipe Sette <felipe_sette@outlook.com>
2020-07-23 15:35:28 -0300

    Aprendendo sobre objetos

--- Works/objetos.js
+++ Works/objetos.js
@@ -1,7 +1,7 @@
- /* Criar um programa que calcule a média das notas entre os alunos, 
- e que mostre a média.*/
+/* Criar um programa que calcule a média das notas entre os alunos, 
+e que mostre a média.*/
 
- const Aluno01 = {
+const Aluno01 = {
   nome: 'Felipe',
   nota: 9.8
 }
@@ -16,12 +16,12 @@ const Aluno03 = {
   nota: 2
 }
 
-const media = ( Aluno01.nota + Aluno02.nota + Aluno03.nota ) / 3
+const media = (Aluno01.nota + Aluno02.nota + Aluno03.nota) / 3
 
 // Se a média for maior que 5, parabenizar a turma
 
 if (media > 5) {
-  console.log (`A média foi de ${media}. Parabéns`)
+  console.log(`A média foi de ${media}. Parabéns`)
 } else {
-  console.log (`A média foi de ${media}. Precisamos melhorar!`)
+  console.log(`A média foi de ${media}. Precisamos melhorar!`)
 }
