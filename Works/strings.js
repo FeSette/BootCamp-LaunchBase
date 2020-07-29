eaa535248ceb84c685f68b1e1ceed68268b2f2da
Felipe Sette <felipe_sette@outlook.com>
2020-07-29 16:27:51 -0300

    Aprendendo sobre Strings

--- Works/strings.js
+++ Works/strings.js
@@ -1,13 +1,13 @@
 
 // String(texto) com aspas duplas:
-const nome = "Felipe"
+const nome = "Felipe";
 // String(texto) com aspas simples: 
-const nome2 = 'Guilherme'  
+const nome2 = 'Guilherme';  
 /* Template String, onde podemos chamar uma outra variável dentro desta
  utilizando ${}. Exemplo: const nome3 = `Fabiana e ${nome}`, assim será 
  impresso, na variável nome3 (Fabiana e Felipe). */
-const nome3 = `Fabiana e ${nome}`                                                        
+const nome3 = `Fabiana e ${nome}`;                                                        
 
 // O comando typeof antes da variável, vai mostrar qual é o tipo da mesma.                                                  
-console.log (typeof nome3)
-console.log (nome3)   
+console.log (typeof nome3);
+console.log (nome3);
