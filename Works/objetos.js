f5a38527c2dd21697753f4e58604d257dc2c948b
Felipe Sette <felipe_sette@outlook.com>
2020-07-30 13:42:58 -0300

    Aprendendo sobre Objetos

--- Works/objetos.js
+++ Works/objetos.js
@@ -4,24 +4,20 @@ e que mostre a média.*/
 const Aluno01 = {
   nome: 'Felipe',
   nota: 9.8
-}
-
+};
 const Aluno02 = {
   nome: 'Guilherme',
   nota: 10
-}
-
+};
 const Aluno03 = {
   nome: 'Henrique',
   nota: 2
-}
-
-const media = (Aluno01.nota + Aluno02.nota + Aluno03.nota) / 3
+};
+const media = (Aluno01.nota + Aluno02.nota + Aluno03.nota) / 3;
 
 // Se a média for maior que 5, parabenizar a turma
-
 if (media > 5) {
-  console.log(`A média foi de ${media}. Parabéns`)
+  console.log(`A média foi de ${media}. Parabéns`);
 } else {
-  console.log(`A média foi de ${media}. Precisamos melhorar!`)
+  console.log(`A média foi de ${media}. Precisamos melhorar!`);
 }
