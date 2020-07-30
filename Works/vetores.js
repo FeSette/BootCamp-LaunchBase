e9ca35cd6568a98b42abf8e6bd473290d2f2de41
Felipe Sette <felipe_sette@outlook.com>
2020-07-30 13:45:55 -0300

    Aprendendo sobre Vetores

--- Works/vetores.js
+++ Works/vetores.js
@@ -14,14 +14,14 @@ const Alunos = [
       nome: 'Henrique',
       nota: 2
   }
-]
+];
 
-const media = ( Alunos[0].nota + Alunos[1].nota + Alunos[2].nota ) / 3
+const media = ( Alunos[0].nota + Alunos[1].nota + Alunos[2].nota ) / 3;
 
 // Se a média for maior que 5, parabenizar a turma
 
 if (media > 5) {
-  console.log (`A média foi de ${media}. Parabéns`)
+  console.log (`A média foi de ${media}. Parabéns`);
 } else {
-  console.log (`A média foi de ${media}. Precisamos melhorar!`)
+  console.log (`A média foi de ${media}. Precisamos melhorar!`);
 }
