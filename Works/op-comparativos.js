e4a214ef1bbaa00731d9f9929494c500af380e25
Felipe Sette <felipe_sette@outlook.com>
2020-07-30 13:36:04 -0300

    Aprendendo sobre Operadores Comparativos

--- Works/op-comparativos.js
+++ Works/op-comparativos.js
@@ -12,22 +12,22 @@
 
 ==============================================================*/
 
-console.log ( 5 > 4 )        // TRUE
-console.log ( 5 < 4 )        // FALSE
-console.log ( 5 >= 4 )       // TRUE
-console.log ( 4 <= 4 )       // FALSE
-console.log ( 4 == "4" )      // TRUE
-console.log ( 4 === "4" )    // FALSE
-console.log ( 4 != "5" )     // TRUE
-console.log ( 4 !== "5" )    // TRUE
+console.log ( 5 > 4 );         // TRUE
+console.log ( 5 < 4 );         // FALSE
+console.log ( 5 >= 4 );        // TRUE
+console.log ( 4 <= 4 );        // FALSE
+console.log ( 4 == "4" );      // TRUE
+console.log ( 4 === "4" );     // FALSE
+console.log ( 4 != "5" );      // TRUE
+console.log ( 4 !== "5" );     // TRUE
 
-const idade = 17
+const idade = 17;
 
 if (idade >= 18) {
-    console.log('Deixar entrar')
+    console.log('Deixar entrar');
 } else {
-    console.log('Bloquear a entrada')
+    console.log('Bloquear a entrada');
 }
 if (idade === 17) {
-    console.log('Volte quando fizer 18 anos')
+    console.log('Volte quando fizer 18 anos');
 }
