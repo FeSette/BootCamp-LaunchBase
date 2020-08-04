161e67f7ecc7252505e42ef27a1988ca3ce983c0
Felipe Sette <felipe_sette@outlook.com>
2020-08-04 11:51:11 -0300

    Desafio 1-1, IMC

--- Challenges/imc.js
+++ Challenges/imc.js
@@ -1,14 +1,14 @@
 // Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
 
-const nome = 'Carlos'
-const peso = 120
-const altura = 1.88
-const sexo = 'Masculino'
+const nome = 'Carlos';
+const peso = 120;
+const altura = 1.88;
+const sexo = 'Masculino';
 
-const IMC = peso / ( altura * altura )
+const imc = peso / ( altura * altura );
 
-if (IMC >= 30){
-    console.log(`${nome}, você está acima do peso.`)
+if (imc >= 30){
+    console.log(`${nome}, você está acima do peso.`);
 } else {
-    console.log(`${nome}, você não está acima do peso.`)
+    console.log(`${nome}, você não está acima do peso.`);
 }
